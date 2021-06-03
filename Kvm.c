e1265d0169a0d3b0d1c66888b7f7d9383b4f3624
Shikha-Pika <deepshikhaswain@gmail.com>
2021-06-03 11:10:40 +0530

    initial commit

---
+++ Kvm.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define NUM_REGS 3
+
+typedef enum
+{
+    OP_LOAD,
+    OP_ADD
+} Opcode;
+
+typedef struct
+{
+    Opcode op;
+    int operands[3];
+} Instruction;
+
+typedef struct
+{
+    int regs[NUM_REGS];
+    Instruction instr;
+} VM;
+
+// printRegister(VM* vm, int i) -> prints the ith register of the VM [vm]
+void printRegister(VM *vm, int i)
+{
+    printf("R_R%d - [ %d ]\n", i, vm->regs[i]);
+}
+
+// runVM(VM* vm, Instruction* ins, int n_ins) -> executes the instructions present in
+//      the buffer [ins]
+void runVM(VM *vm, Instruction *ins, int numInstrs)
+{
+    for (int i = 0; i < numInstrs; i++)
+    {
+        const Instruction instr = ins[i];
+        switch (instr.op)
+        {
+        case OP_LOAD:
+        {
+            const int r_no = instr.operands[1];
+            const int value = instr.operands[0];
+            vm->regs[r_no] = value;
+            break;
+        }
+        // case OP_ADD:
+        //     j = ins->operands[2];
+        //     vm->regs[j] = vm->regs[1] + vm->regs[0];
+        default:
+            fprintf(stderr, "Invalid Opcode!");
+            exit(1);
+            break;
+        }
+    }
+}
+
+int main()
+{
+    VM vm;
+    Instruction ins[3];
+    vm.regs[0] = 0;
+    vm.regs[1] = 0;
+    vm.regs[2] = 0;
+
+    ins[0].op = OP_LOAD;
+    ins[0].operands[0] = 10;
+    ins[0].operands[1] = 0;
+
+    ins[1].op = OP_LOAD;
+    ins[1].operands[0] = 20;
+    ins[1].operands[1] = 1;
+
+    // ins[2].op = OP_ADD;
+    // ins[2].operands[0] = 0;
+    // ins[2].operands[1] = 1;
+    // ins[2].operands[2] = 2;
+
+    runVM(&vm, ins, 2);
+    printRegister(&vm, 0); // 10
+    printRegister(&vm, 1); // 20
+}
---
+++ Notes.md
@@ -0,0 +1,18 @@
+# Notes for implementation of KVM
+
+## Feature-set:
+- Can handle strings, booleans, numbers, arrays and maps.
+- Is a register based VM.
+
+
+## Instruction set:
+
+Example program:
+
+LOAD 10 $0
+LOAD 20 $1
+ADD $0 $1 $2
+
+git remote add origin https://github.com/Shikha-Pika/KVM.git
+git branch -M main
+git push -u origin main
