c68d460d1158f72d589f1913b29eeebc3e3915d9
srijan-paul <srijannr1@gmail.com>
2021-06-04 16:19:10 +0530

    format main.c

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ int main()
     vm.regs[2] = 0;
 
     Instruction ins[3] = {
-    	/* LOAD 10 $1 */
-	    opAB(OP_LOAD, 10, 1), 
-	    /* LOAD 20 $2 */
-	    opAB(OP_LOAD, 20, 2), 
-    	/* ADD $2 $2 $1 */
-	    opABC(OP_ADD, 2, 2, 1), 
+        /* LOAD 10 $1 */
+        opAB(OP_LOAD, 10, 1),
+        /* LOAD 20 $2 */
+        opAB(OP_LOAD, 20, 2),
+        /* ADD $2 $2 $1 */
+        opABC(OP_ADD, 2, 2, 1),
 
-    }; 
+    };
 
     runVM(&vm, ins, 3);
-    printRegister(&vm, 0); // 10
-    printRegister(&vm, 1); // 20
-    printRegister(&vm, 2); //30
+    printRegister(&vm, 0);
+    printRegister(&vm, 1);
+    printRegister(&vm, 2);
 }
